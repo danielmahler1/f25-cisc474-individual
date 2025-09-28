cf6c6473ce56a13cc5f061b9eb91916a947fdc98
danielmahler1 <danielmahler34@gmail.com>
2025-09-27 21:45:33 -0400

    added all controllers, services, and modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 
 import { LinksModule } from './links/links.module';
+import { AssignmentsModule } from './assignments/assignments.module';
+import { CalendarEventsModule } from './calendar-events/calendar-events.module';
+import { CoursesModule } from './courses/courses.module';
+import { EnrollmentsModule } from './enrollments/enrollments.module';
+import { SubmissionsModule } from './submissions/submissions.module';
+import { UsersModule } from './users/users.module';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [LinksModule],
+  imports: [
+    LinksModule,
+    AssignmentsModule,
+    CalendarEventsModule,
+    CoursesModule,
+    EnrollmentsModule,
+    SubmissionsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
